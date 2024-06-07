666407dee41dc4877243caa57cf9f0d2d340003d
venkatakrishna1411 <thewhitedevil404@gmail.com>
2024-06-07 11:50:34 +0530

    initial commit

---
+++ UniversityService/src/test/java/com/eidiko/EidikoUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eidiko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EidikoUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
