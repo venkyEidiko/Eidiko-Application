26db81fd7c41566620680c5ca998ea23fe9514bc
Nkyadav650 <nkyadav650@gmail.com>
2024-06-11 21:35:09 -0700

    created generic table and error component

--- UniversityApplication/src/app/app.module.ts
+++ UniversityApplication/src/app/app.module.ts
@@ -1,14 +1,33 @@
+;
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './error/error.component';
+//import { AppRoutingRoutingModule } from './app-routing-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { TableComponent } from './table/table.component';
+import { MatPaginatorModule} from '@angular/material/paginator';
+import { MatTableModule} from '@angular/material/table';
+const routes: Routes = [
+  //{path:'table',component:TableComponent},
+  {path:'**',component:ErrorComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatPaginatorModule,MatTableModule
+    
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
