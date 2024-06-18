4403d62f29e056f4f8b1aed3adca21272bbaf186
ShreyaSuddala <shreyasuddala27@gmail.com>
2024-06-18 11:36:25 +0530

    created profile page

---
+++ UniversityApplication/src/app/about-timeline/about-timeline.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-timeline',
+  templateUrl: './about-timeline.component.html',
+  styleUrls: ['./about-timeline.component.css']
+})
+export class AboutTimelineComponent {
+
+}
---
+++ UniversityApplication/src/app/profile-prfl/profile-prfl.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-prfl',
+  templateUrl: './profile-prfl.component.html',
+  styleUrls: ['./profile-prfl.component.css']
+})
+export class ProfilePrflComponent {
+
+}
